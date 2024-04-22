d116d29a0ac5c0b7edfb879ee6b43663fe6b79a0
Kaushik Sengupta <105369038+KaushikS19@users.noreply.github.com>
2024-04-22 18:33:27 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Kaushik"
+let age = 19
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
