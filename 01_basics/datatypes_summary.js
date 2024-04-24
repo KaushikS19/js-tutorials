7a9e9e9b11858b208d79956d09fbfd1418257c19
Kaushik Sengupta <105369038+KaushikS19@users.noreply.github.com>
2024-04-24 05:25:37 +0000

    datatypes summary start

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,9 @@
+// Primitive datatypes => call by value (copy)
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+// Non primitive datatypes => call by Reference
+
+// Array, Objects, Functions
