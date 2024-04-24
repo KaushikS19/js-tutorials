8be28e6565f74b039662a663ed877ad4386908f3
Kaushik Sengupta <105369038+KaushikS19@users.noreply.github.com>
2024-04-24 04:42:45 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Kaushik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+
+// 1 => true; 0 => false
+// "" => false; "kaushik" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
